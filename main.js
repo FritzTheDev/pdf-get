81ee158d468bb79d46c54843c1ce8dc684541fc5
ottovbismarck <fritzj96@gmail.com>
2018-10-24 03:22:20 -0700

    More tinkering

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
 
 //get electron properties via destructuring
 const { app, BrowserWindow, Menu, ipcMain } = electron;
@@ -12,7 +13,7 @@ let mainWindow;
 app.on('ready', () => {
 
   //create new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({height: 800, width: 1200});
 
   //load html into window
   mainWindow.loadURL(url.format({
@@ -30,7 +31,10 @@ app.on('ready', () => {
 
 ipcMain.on('item:add', (e, item) => {
   console.log(item);
-})
+  fs.writeFileSync('config.json', item, (err) => {
+    console.log('Error adding to list. Contact Fritz');
+  });
+});
 
 //main menu template
 const mainMenuTemplate = [

dd7a082efcd2380331b66b5f7c0f722fb52ffc85
ottovbismarck <fritzj96@gmail.com>
2018-10-24 07:46:06 -0700

    Pre-release version #1 of 3

--- pdf-getter-stuff/pdf-getter-script.js
+++ pdf-getter-stuff/pdf-getter-script.js
@@ -1,13 +1,33 @@
 const electron = require('electron');
 const fs = require('fs');
-fs.readFile('target-list.json', (err, data) => {
-  if (err) throw err;
-  const parsedJson = (JSON.parse(data));
-  const contentList = document.getElementById("contentList");
+const request = require('request');
+
+
+const contentList = document.getElementById("contentList");
+const getPdfButton = document.getElementById("getPdfBtn");
+// Populates the list with entries
+function populateList() {
+  const parsedJson = getListObjs();
   parsedJson.map(x => {
     const button = document.createElement('button');
     button.classList.add('list-group-item', 'list-group-action');
     button.textContent = x.resource_name;
     contentList.appendChild(button);
   });
-});
+}
+function getPdfs() {
+  const today = new Date();
+  const list = getListObjs();
+  fs.mkdirSync(`./temp/${today.toDateString()}`);
+  list.map(x => {
+    request(x.url).pipe(fs.createWriteStream(`./temp/${today.toDateString()}/${x.resource_name}.pdf`));
+  })
+}
+
+getPdfButton.addEventListener("click", () => {getPdfs()});
+populateList();
+
+//helper function that gets & parses the list of targeted pdfs
+function getListObjs() {
+  return JSON.parse(fs.readFileSync('target-list.json', 'utf-8'));
+}
